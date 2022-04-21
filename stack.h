006d9965c9f8eb3b5993fcd0cf6e5aefb0473753
btruj007 <102835844+btruj007@users.noreply.github.com>
2022-04-21 08:41:25 -0700

    Update stack.h

--- stack.h
+++ stack.h
@@ -4,19 +4,58 @@
 
 using namespace std;
 
+const int MAX_SIZE = 20;
+
 template<typename T>
 class stack 
 {
 private:
-	T data;
+	T data[MAX_SIZE];
 	int size;
 
 public:
-	stack();
-	void push(T val);
-	void pop();
-	void pop_two();
-	T top();
-	void empty();
+	stack(){
+		size = 0;
+	}
+
+	void push(T val){
+		if (size < 20)
+		{
+			size++;
+			data[20 - size] = val;
+		}
+		else
+		{
+			throw overflow_error("Called push on full stack.");
+		}
+	}
+
+	void pop(){
+
+	}
+
+	void pop_two() {
+
+	}
+
+	T top() {
+		if (size > 0)
+		{
+			return data[20 - size];
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	bool empty() {
+		if (size == 0)
+		{
+			return true;
+		}
+
+		return false;
+	}
 
 };
